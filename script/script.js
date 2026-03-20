b5bab540d2782eed87251c524f9b365f852e8f74
Mateo Betanzo <mateobetanzo@MacBook-Pro-de-Mateo.local>
2026-03-20 16:17:14 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,74 @@
+const coords = { x: 0, y: 0 }
+const circles = document.querySelectorAll('.circle')
+const firstCircle = document.querySelector('#uno')
+let isClicked = false
+
+// const openMenu = document.querySelector('.open-menu')
+// const closeMenu = document.querySelector('.close-menu')
+// const menu = document.querySelector('.list-menu')
+
+// openMenu.addEventListener('click', () => {
+//     menu.classList.add('active')
+//     openMenu.style.visibility = 'hidden'
+// })
+
+// closeMenu.addEventListener('click', () => {
+//     menu.classList.remove('active')
+//     openMenu.style.visibility = 'visible'
+// })
+
+
+
+
+
+
+circles.forEach(function(circle){
+    circle.x = 0
+    circle.y = 0
+})
+
+window.addEventListener('mousemove', function(e) {
+    coords.x = e.clientX
+    coords.y = e.clientY
+})
+
+window.addEventListener("mousedown", () => {
+    isClicked = true
+});
+
+window.addEventListener("mouseup", () => {
+    isClicked = false
+});
+
+function animatedCircles() {
+    const RADIUS = 12
+    let x = coords.x
+    let y = coords.y
+
+    circles.forEach(function(circle, index) {
+        circle.style.left = (x - RADIUS) + 'px'
+        circle.style.top = (y - RADIUS) + 'px'
+
+        firstCircle.style.left = x - 20 + 'px'
+        firstCircle.style.top = y - 20 + 'px'
+
+        circle.style.scale = (circles.length - index) / circles.length
+
+        if (isClicked) {
+            firstCircle.style.visibility = 'visible'
+        } else {
+            firstCircle.style.visibility = 'hidden'
+        }
+
+        circle.x = x
+        circle.y = y
+
+        const nextCircle = circles[index + 1] || circles[0]
+        x += (nextCircle.x - x) * 0.3
+        y += (nextCircle.y - y) * 0.3
+    })
+
+    requestAnimationFrame(animatedCircles)
+}
+
+animatedCircles()
